4996ebc471d4157c4421f0a802a1d5a9a0ecd94a
anarouanet <anarouanet@gmail.com>
2023-11-07 14:02:19 +0100

    requirename plotrix

--- R/myheatDissmat.R
+++ R/myheatDissmat.R
@@ -1,5 +1,9 @@
 myheatDissMat <- function (dissimObj, main = NULL, xlab = NULL, ylab = NULL)
 {
+
+if (!requireNamespace("plotrix", quietly = TRUE)) {
+  stop("The package {plotrix} is required for this action")
+}
   nSbj <- dissimObj$disSimRunInfoObj$nSubjects
   col.labels <- c("0", "0.5", "1")
   colours <- colorRampPalette(c("white", "black"))(10)
@@ -8,6 +12,6 @@ myheatDissMat <- function (dissimObj, main = NULL, xlab = NULL, ylab = NULL)
   heatmap(1 - dissMat, keep.dendro = FALSE, symm = TRUE, Rowv = NULL,
           labRow = FALSE, labCol = FALSE, margins = c(4.5, 4.5),
           col = colours, main = main, xlab = xlab, ylab = ylab)
-  plotrix::color.legend(0.95, 0.7, 1, 1, legend = col.labels, colours,
+  color.legend(0.95, 0.7, 1, 1, legend = col.labels, colours,
                         gradient = "y", align = "rb")
 }
