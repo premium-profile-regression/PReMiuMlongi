46bbee2e37e58ca86704a594694397204b8bcf4c
anarouanet <anarouanet@gmail.com>
2026-05-05 00:10:56 +0200

    Rpintf instead of printf

--- src/include/PReMiuMModel.h
+++ src/include/PReMiuMModel.h
@@ -4259,7 +4259,7 @@ double logCondPostThetaBeta(const pReMiuMParams& params,
     //   }
     // }
   }else{
-    printf("should not use this function when outcome = LME");
+    Rprintf("should not use this function when outcome = LME");
   }
   // Prior for beta
   // There were no fixed effects in the Molitor paper but to be consistent with
