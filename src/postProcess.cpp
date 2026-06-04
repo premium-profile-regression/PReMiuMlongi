3a1d3a7b7ed155be051a6b7ba263d6e867b3acf8
anarouanet <anarouanet@gmail.com>
2026-06-04 11:03:46 +0200

    Rprintf(%d)

--- src/postProcess.cpp
+++ src/postProcess.cpp
@@ -52,7 +52,7 @@ SEXP calcDisSimMat(SEXP fileName, SEXP nSweeps, SEXP nBurn, SEXP nFilter,SEXP nS
     		}
      	}else{
     		if((1+k-firstLine)==1||(1+k-firstLine)%1000==0){
-				Rprintf("Stage 1:%lu samples out of %lu\n",(unsigned long) (1+k-firstLine),(unsigned long) (1+nLines-firstLine));
+				Rprintf("Stage 1:%d samples out of %d \n",(int) (1+k-firstLine),(int) (1+nLines-firstLine));
 			}
 			for(unsigned long int i=0;i<nSj+nPSj;i++){
     			// Fill up the cluster data for this sweep
@@ -95,7 +95,7 @@ SEXP calcDisSimMat(SEXP fileName, SEXP nSweeps, SEXP nBurn, SEXP nFilter,SEXP nS
         	}
         }else{
         	if((1+k-firstLine)==1||(1+k-firstLine)%1000==0){
-			Rprintf("Stage 2:%lu samples out of %lu\n", (unsigned long) (1+k-firstLine), (unsigned long) (1+nLines-firstLine));
+			Rprintf("Stage 2:%d samples out of %d \n", (int) (1+k-firstLine), (int) (1+nLines-firstLine));
     		}
         	for(unsigned long int i=0;i<nSj+nPSj;i++){
         		// Fill up the cluster data for this sweep
